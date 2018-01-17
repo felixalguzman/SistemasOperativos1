399ba3085979a682b707671bd0a50418e39109c0
felixalguzman <felixlvl@gmail.com>
2018-01-16 21:03:32 -0400

    Modificando punto 6

--- Tarea 2/punto7.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ruta="/home/felixal/Documents/Sistemas Operativos/SistemasOperativos1/Tarea 2/Mail/Inbox"
-archivo="agenda.txt"
-cd "$ruta"
-echo "" > $archivo
-chmod u+rw $archivo
-chmod g+r $archivo
-chmod o-r $archivo
