5a3d5cba45656bbb6cbe7b71be768a5d6aa3d228
felixalguzman <felixlvl@gmail.com>
2018-01-22 15:02:32 -0400

    Arreglando el script 1

--- Tarea 2/punto1.sh
+++ Tarea 2/punto1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
-p1=felixp1.txt
-echo "Punto 1" >> $p1
-ls ??a* >> $p1
-echo "Punto 1 parte 2" >> $p1
-ls -a [aeiou]* ; ls -a [0-9]* >> $p1
-echo "Punto 1 parte 3" >> $p1
-ls | grep [b*][0-9] >> $p1
+cd /bin
+p1="/home/felixal/Documents/Sistemas Operativos/SistemasOperativos1/Tarea 2/felixp1.txt"
+echo "Punto 1" >> "$p1"
+ls ??a* >> "$p1"
+echo "Punto 1 parte 2" >> "$p1"
+ls -a [aeiou]* >> "$p1"; ls -a [0-9]* >> "$p1"
+echo "Punto 1 parte 3" >> "$p1"
+ls | grep [b*][0-9] >> "$p1"
+cd -
